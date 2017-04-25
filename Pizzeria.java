000dc024f8d0df8c2f9efc8d63df5f702ef108d2
rdt-01 <r.torres10@ufromail.cl>
2017-04-25 12:44:03 -0300

    Update Pizzeria.java

--- Pizzeria.java
+++ Pizzeria.java
@@ -17,7 +17,7 @@ public class Pizzeria {
         int cantPizzaA, cantPizzaB, cantPizzaC, totalPizzaA = 0, totalPizzaB = 0, totalPizzaC = 0;
         int stockBases, stockQueso, stockJamon, stockTomate, stockChamp;
         int stock_finalBases, stock_finalQueso, stock_finalJamon, stock_finalTomate, stock_finalChamp;
-        int nuevaOrden, ii = 1, iii = 1, i = 1;
+        int nuevaOrden, ii = 1, iii = 1, i = 1, montoA = 0, montoB = 0, montoC = 0, montoTotal = 0;
 
         //ENTRADAS
         //Ingreso de stock inicial de ingredientes
@@ -95,103 +95,109 @@ public class Pizzeria {
         } while ((champ < 5) || (champ > 10));
 
         //Ventas
+        System.out.println("\n - - - VENTA - - - ");
+        System.out.println("Bienvenido. ¿Qué desea llevar? (Ingrese el número de su orden)");
+        
+        
+        System.out.println(" 1. Pizza A   -   2. Pizza B   -   3. Pizza C   - - -   4. Pagar  -  5. Cuadrar caja");
+        System.out.println(" - Queso          - Queso          - Queso"
+                + "\n - Tomate         - Tomate         - Jamón"
+                + "\n - Jamón          - Champiñones    - Champiñones"
+                + "\n $2500            $3000            $3500");
+
         do {
 
-            System.out.println("\n - - - VENTA - - - ");
-            System.out.println("Bienvenido. ¿Qué desea llevar? (Ingrese el número de su orden)");
+            tipoPizza = entrada.nextInt();
 
-            do {
+            if ((tipoPizza < 1) || (tipoPizza > 5)) {
+                System.out.println("\nOrden no válida, ingrese nuevamente");
+            }
 
-                System.out.println(" 1. Pizza A   -   2. Pizza B   -   3. Pizza C");
-                System.out.println(" - Queso          - Queso          - Queso"
-                        + "\n - Tomate         - Tomate         - Jamón"
-                        + "\n - Jamón          - Champiñones    - Champiñones"
-                        + "\n $2500            $3000            $3500");
+        } while ((tipoPizza) < 1 || (tipoPizza > 5));
 
+        switch (tipoPizza) {
+
+            case 1:
+                System.out.println("¿Cuántas pizzas de tipo A desea llevar?");
+                cantPizzaA = entrada.nextInt();
+                totalPizzaA = totalPizzaA + cantPizzaA;
                 
+                montoA = montoA + (cantPizzaA * 2500);
+
+                stockBases = stockBases - cantPizzaA;
+                stockQueso = stockQueso - 100;
+                stockJamon = stockJamon - 100;
+                stockTomate = stockTomate - 200;
+
                 do {
 
-                    tipoPizza = entrada.nextInt();
+                    System.out.println("¿Desea agregar otra orden?");
+                    System.out.println(" 1. Sí   -   2. No ");
+                    nuevaOrden = entrada.nextInt();
 
-                    if ((tipoPizza < 1) || (tipoPizza > 3)) {
-                        System.out.println("\nOrden no válida, ingrese nuevamente");
+                    if ((nuevaOrden < 1) || (nuevaOrden > 2)) {
+                        System.out.println("Opción no válida, ingrese nuevamente");
                     }
 
-                } while ((tipoPizza) < 1 || (tipoPizza > 3));
-
-                do {
-
-                    switch (tipoPizza) {
+                    switch (nuevaOrden) {
 
                         case 1:
-                            System.out.println("¿Cuántas pizzas de tipo A desea llevar?");
-                            cantPizzaA = entrada.nextInt();
-                            totalPizzaA = totalPizzaA + cantPizzaA;
-
-                            stockBases = stockBases - cantPizzaA;
-                            stockQueso = stockQueso - 100;
-                            stockJamon = stockJamon - 100;
-                            stockTomate = stockTomate - 200;
-
-                            do {
-
-                                System.out.println("¿Desea agregar otra orden?");
-                                System.out.println(" 1. Sí   -   2. No ");
-                                nuevaOrden = entrada.nextInt();
-
-                                if ((nuevaOrden < 1) || (nuevaOrden > 2)) {
-                                    System.out.println("Opción no válida, ingrese nuevamente");
-                                }
-
-                                switch (nuevaOrden) {
-
-                                    case 1:
-                                        ii++;
-                                        
-                                        
-                                    case 2:
-                                        i = 0;
-                                        ii = 0;
-                                }
-
-                            } while ((nuevaOrden < 1) || (nuevaOrden > 2));
-
-                            break;
 
                         case 2:
-                            System.out.println("¿Cuántas pizzas de tipo B desea llevar?");
-                            cantPizzaB = entrada.nextInt();
-                            totalPizzaB = totalPizzaB + cantPizzaB;
-                            stock_finalBases = stockBases - cantPizzaB;
-
-                            stockBases = stockBases - cantPizzaB;
-                            stockQueso = stockQueso - 200;
-                            stockTomate = stockTomate - 200;
-                            stockChamp = stockChamp - 100;
-
-                            break;
-
-                        case 3:
-                            System.out.println("¿Cuántas pizzas de tipo C desea llevar?");
-                            cantPizzaC = entrada.nextInt();
-                            totalPizzaC = totalPizzaC + cantPizzaC;
-                            stock_finalBases = stockBases - cantPizzaC;
-
-                            stockBases = stockBases - cantPizzaC;
-                            stockQueso = stockQueso - 200;
-                            stockJamon = stockJamon - 200;
-                            stockChamp = stockChamp - 200;
-
-                            break;
-
+                           
                     }
-                } while (i > 0);
 
-            } while (ii > 0);
+                } while ((nuevaOrden < 1) || (nuevaOrden > 2));
 
-        } while (iii > 0);
+                break;
+
+            case 2:
+                System.out.println("¿Cuántas pizzas de tipo B desea llevar?");
+                cantPizzaB = entrada.nextInt();
+                totalPizzaB = totalPizzaB + cantPizzaB;
+                stock_finalBases = stockBases - cantPizzaB;
+                
+                montoB = montoB + (cantPizzaB * 3000);
+
+                stockBases = stockBases - cantPizzaB;
+                stockQueso = stockQueso - 200;
+                stockTomate = stockTomate - 200;
+                stockChamp = stockChamp - 100;
+
+                break;
+
+            case 3:
+                System.out.println("¿Cuántas pizzas de tipo C desea llevar?");
+                cantPizzaC = entrada.nextInt();
+                totalPizzaC = totalPizzaC + cantPizzaC;
+                stock_finalBases = stockBases - cantPizzaC;
+                
+                montoC = montoC + (cantPizzaC * 3500);
+
+                stockBases = stockBases - cantPizzaC;
+                stockQueso = stockQueso - 200;
+                stockJamon = stockJamon - 200;
+                stockChamp = stockChamp - 200;
+
+                break;
+                
+             case 4:
+                 //Salir del ciclo para el reporte de cierre de caja
+
+        }
 
         System.out.println(" - - - REPORTE CIERRE DE CAJA - - -");
+        
+        //Cantidad de pizzas vendidas, monto total
+        int totalPizzas;
+        totalPizzas = totalPizzaA + totalPizzaB + totalPizzaC;
+        System.out.println("CANTIDAD DE PIZZAS VENDIDAS: "+totalPizzas+" ($"+montoTotal+")");
+        //Cuantas pizzas tipo x se vendieron, monto por cada tipo
+        System.out.println("DETALLE DE PIZZAS VENDIDAS: "+totalPizzaA+" pizzas de tipo A ($"+montoA+"), "+totalPizzaB+" pizzas de tipo B ($"+montoB+"),"
+                + " "+totalPizzaC+" pizzas de tipo C ($"+montoC+").");
+        //Cuanto ingrediente se vendio, mostrando el stock restante
+        System.out.println("STOCK RESTANTE: "+stockBases+" bases, "+stockQueso+" queso,"+stockJamon+" jamon,"+stockTomate+" tomates,"+stockChamp+" champiñones.");
+        //Utilidad del dia total. (Utilidad = ingresos - Costos)
 
     }
 
